12c8a788a19125586262072ad0f60630225fb983
BrandonPacewic <92102436+BrandonPacewic@users.noreply.github.com>
2026-02-06 01:36:23 -0800

    fix: update high range to 256

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -2859,7 +2859,7 @@ static ssize_t qos_batch_limit_store(struct device *dev,
 
 	if (kstrtouint(buf, 10, &val) < 0)
 		return -EINVAL;
-	if (val < 1 || val > 32)
+	if (val < 1 || val > 256)
 		return -EINVAL;
 
 	ndev->qos_batch_limit = val;
